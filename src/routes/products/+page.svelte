ecc98086afacf44bf1a09c20e2f48fffd602b9c7
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-27 15:31:00 +0700

    Page Data

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,1 +1,12 @@
-<h1>List of products</h1>
+<script>
+    export let data;
+    const products = data.products
+</script>
+<h1>{ data.title }</h1>
+{#each  products as product}
+    <div>
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+    </div>
+    <hr>
+{/each}
