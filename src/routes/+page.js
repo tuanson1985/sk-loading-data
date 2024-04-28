c29961099d31d58a3876cbaa3b62f8f0f077c3ae
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-04-28 23:44:45 +0700

    Pre-rendering API Routes

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,8 @@
-export const load = async () => {
+export const load = async ({ fetch }) => {
 	console.log('home page universal load funciton called');
+	const response = await fetch('/api/current-time');
+	const currentTime = await response.text();
+	return { currentTime };
 };
 
 export const prerender = true;
