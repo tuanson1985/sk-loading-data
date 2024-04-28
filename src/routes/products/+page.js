9025d9222d28aca490da069e1136a72486e4ff37
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-28 09:51:30 +0700

    Universal and server load functions

--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -2,10 +2,7 @@ import Product from './product.svelte';
 
 export const load = async (loadEvent) => {
     console.log("Load function called in page.js");
-    const { fetch, data } = loadEvent;
-    const title = "List of available products";
-    const response = await fetch("http://localhost:4000/products");
-    const products = await response.json();
+    const { data } = loadEvent;
     return {
         ...data,
         Component: Product
