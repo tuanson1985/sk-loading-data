248e58542211d21cf6f0cfde28772e3e12604ce3
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-04-28 23:07:31 +0700

    SSR and CSR

--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -1,12 +1,15 @@
 import Product from './product.svelte';
 
 export const load = async (loadEvent) => {
-    console.log("Load function called in page.js");
-    const notification = 'End of season sale';
-    const { data } = loadEvent;
-    return {
-        ...data,
-        Component: Product,
-        notification,
-    };
-}
+	console.log('Load function called in page.js');
+	const notification = 'End of season sale';
+	const { data } = loadEvent;
+	return {
+		...data,
+		Component: Product,
+		notification
+	};
+};
+
+export const ssr = true;
+export const csr = true;
