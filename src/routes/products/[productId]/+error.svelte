8d692ebe9652e53c504091742b488be489e8ae47
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-28 10:40:27 +0700

    Errors and Redirects

--- src/routes/products/[productId]/+error.svelte
+++ src/routes/products/[productId]/+error.svelte
@@ -1,6 +1,6 @@
 <script>
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
 </script>
 
 <h1>{$page.status}: {$page.error.message}</h1>
-<a href={$page.error.url}>Quay lại</a>
+<p>{$page.error.hint}</p>
--- src/routes/products/[productId]/+page.server.js
+++ src/routes/products/[productId]/+page.server.js
@@ -1,17 +1,12 @@
-// import { error } from "@sveltejs/kit";
-import { redirect } from "@sveltejs/kit";
+import { error } from '@sveltejs/kit';
 
 export const load = async (serverLoadEvent) => {
     const { fetch, params } = serverLoadEvent;
     const { productId } = params;
-    if(productId > 3) {
-        // throw error(404, 'Product not found');//tra ve chuoi
-        // throw error(404, {
-        //     message: 'Product not found',
-        //     url: 'products'
-        // });
-        throw redirect(307, '/products');
-    }
+    if (productId > 3) {
+		throw error(404, { message: 'Product not found', hint: 'Try a different product' });
+		// throw redirect(307, '/products');
+	}
     const title = "Product details";
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const product = await response.json();
