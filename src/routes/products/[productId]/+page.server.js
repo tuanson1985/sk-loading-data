fc6ffe0dd598b9adb617cf3cf50b51d0b09be5d4
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-28 10:29:31 +0700

    Errors and Redirects

--- src/routes/products/[productId]/+page.server.js
+++ src/routes/products/[productId]/+page.server.js
@@ -1,7 +1,17 @@
+// import { error } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
+
 export const load = async (serverLoadEvent) => {
-    const { fetch, params, url, route } = serverLoadEvent;
-    console.log(params, url, route);
+    const { fetch, params } = serverLoadEvent;
     const { productId } = params;
+    if(productId > 3) {
+        // throw error(404, 'Product not found');//tra ve chuoi
+        // throw error(404, {
+        //     message: 'Product not found',
+        //     url: 'products'
+        // });
+        throw redirect(307, '/products');
+    }
     const title = "Product details";
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const product = await response.json();
