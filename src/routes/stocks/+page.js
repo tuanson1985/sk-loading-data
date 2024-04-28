0ae6fa59ebf68f4a4b6e7caac21e7255d5e4cbe3
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-28 15:29:17 +0700

     Data Invalidation

--- src/routes/stocks/+page.js
+++ src/routes/stocks/+page.js
@@ -1,14 +1,7 @@
 export const load = async (loadEvent) => {
-	const { fetch } = loadEvent;
-	const mostActiveStockResponse = await fetch('http://localhost:4000/most-active-stock');
-	const topGainingStockResponse = await fetch('http://localhost:4000/top-gaining-stock');
-	const topLosingStockResponse = await fetch('http://localhost:4000/top-losing-stock');
-	// const mostActiveStock = await mostActiveStockResponse.json();
-	// const topGainingStock = await topGainingResponse.json();
-	// const topLosingStock = await topLosingResponse.json();
-	return {
-		mostActiveStock: mostActiveStockResponse.json(),
-		topGainingStock: topGainingStockResponse.json(),
-		topLosingStock: topLosingStockResponse.json()
-	};
+	const { fetch, depends } = loadEvent;
+	depends('stocks:actively-trading');
+	const response = await fetch('http://localhost:4000/stocks');
+	const stocks = await response.json();
+	return { stocks };
 };
--- src/routes/stocks/+page.svelte
+++ src/routes/stocks/+page.svelte
@@ -1,15 +1,17 @@
 <script>
+	import { invalidateAll, invalidate } from '$app/navigation';
 	export let data;
+
+	function refresh() {
+		// invalidateAll();
+		invalidate('stocks:actively-trading');
+	}
 </script>
 
-<span>Most active stock - {data.mostActiveStock.symbol}</span>
-<span>Top gaining stock - {data.topGainingStock.symbol}</span>
-<span>Top losing stock - {data.topLosingStock.symbol}</span>
-<slot />
+<h1>Actively trading stocks</h1>
 
-<style>
-	span {
-		margin-right: 30px;
-		font-size: 24px;
-	}
-</style>
+{#each data.stocks as stock}
+	<h2>{stock.symbol} - ${stock.price}</h2>
+{/each}
+
+<button on:click={refresh}>Refresh</button>
